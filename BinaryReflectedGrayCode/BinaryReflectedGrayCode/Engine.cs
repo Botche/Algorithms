45fcd02bf42f4026c0b5d614d8e3b1434b504ca7
Gabriel Petkov <47752724+Botche@users.noreply.github.com>
2021-03-20 19:56:12 +0200

    Add binary reflected gray code algoritm

---
+++ BinaryReflectedGrayCode/BinaryReflectedGrayCode/Engine.cs
@@ -0,0 +1,48 @@
+﻿namespace BinaryReflectedGrayCode
+{
+    using System;
+    using System.Text;
+
+    public class Engine
+    {
+        private readonly StringBuilder stringBuilder;
+
+		public Engine()
+		{
+            this.stringBuilder = new StringBuilder();
+        }
+         
+        public void Run()
+        {
+            Console.Write("Please enter a number: ");
+            int depth = int.Parse(Console.ReadLine());
+
+            Generate(depth);
+        }
+
+        private void Generate(int depth)
+        {
+            int initalValue = 1;
+            RecursiveGenerator(initalValue, string.Empty, depth);
+
+            string trimmedResult = this.stringBuilder.ToString().TrimEnd();
+            Console.WriteLine(trimmedResult);
+        }
+
+        private void RecursiveGenerator(int number, string prevValue, int limit)
+        {
+            bool isNumberOverLimit = number > limit;
+            if (isNumberOverLimit)
+            {
+                return;
+            }
+
+            prevValue += number + prevValue;
+
+            this.stringBuilder.AppendLine(prevValue);
+            number++;
+
+            RecursiveGenerator(number, prevValue, limit);
+        }
+    }
+}

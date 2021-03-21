4530be594d314428cd262372edce61a2ff641606
Gabriel <47752724+Botche@users.noreply.github.com>
2021-03-21 21:17:03 +0200

    Add not recursive solution of algorithm

--- BinaryReflectedGrayCode/BinaryReflectedGrayCode/Engine.cs
+++ BinaryReflectedGrayCode/BinaryReflectedGrayCode/Engine.cs
@@ -7,11 +7,11 @@
     {
         private readonly StringBuilder stringBuilder;
 
-		public Engine()
-		{
+        public Engine()
+        {
             this.stringBuilder = new StringBuilder();
         }
-         
+
         public void Run()
         {
             Console.Write("Please enter a number: ");
@@ -23,26 +23,18 @@
         private void Generate(int depth)
         {
             int initalValue = 1;
-            RecursiveGenerator(initalValue, string.Empty, depth);
+
+            string previousValue = string.Empty;
+            for (int number = initalValue; number <= depth; number++)
+            {
+                previousValue += number + previousValue;
+
+                this.stringBuilder.AppendLine(previousValue);
+                number++;
+            }
 
             string trimmedResult = this.stringBuilder.ToString().TrimEnd();
             Console.WriteLine(trimmedResult);
-        }
-
-        private void RecursiveGenerator(int number, string prevValue, int limit)
-        {
-            bool isNumberOverLimit = number > limit;
-            if (isNumberOverLimit)
-            {
-                return;
-            }
-
-            prevValue += number + prevValue;
-
-            this.stringBuilder.AppendLine(prevValue);
-            number++;
-
-            RecursiveGenerator(number, prevValue, limit);
         }
     }
 }
---
+++ BinaryReflectedGrayCode/BinaryReflectedGrayCodeRecursive/Engine.cs
@@ -0,0 +1,48 @@
+﻿namespace BinaryReflectedGrayCodeRecursive
+{
+    using System;
+    using System.Text;
+
+    public class Engine
+    {
+        private readonly StringBuilder stringBuilder;
+
+		public Engine()
+		{
+            this.stringBuilder = new StringBuilder();
+        }
+         
+        public void Run()
+        {
+            Console.Write("Please enter a number: ");
+            int depth = int.Parse(Console.ReadLine());
+
+            Generate(depth);
+        }
+
+        private void Generate(int depth)
+        {
+            int initalValue = 1;
+            RecursiveGenerator(initalValue, string.Empty, depth);
+
+            string trimmedResult = this.stringBuilder.ToString().TrimEnd();
+            Console.WriteLine(trimmedResult);
+        }
+
+        private void RecursiveGenerator(int number, string previousValue, int limit)
+        {
+            bool isNumberOverLimit = number > limit;
+            if (isNumberOverLimit)
+            {
+                return;
+            }
+
+            previousValue += number + previousValue;
+
+            this.stringBuilder.AppendLine(previousValue);
+            number++;
+
+            RecursiveGenerator(number, previousValue, limit);
+        }
+    }
+}
